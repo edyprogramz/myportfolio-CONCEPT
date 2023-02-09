8a5269269b542d621bc36c0e6d0dceabf32ca1d4
edyprogramz <116636391+edyprogramz@users.noreply.github.com>
2023-02-09 13:35:32 +0300

    js, html/css

---
+++ index.js
@@ -0,0 +1,13 @@
+var tabcontents = document.getElementsByClassName('tab-contents');
+var tablinks = document.getElementsByClassName('tab-links');
+
+function opentab(tabname){
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab');
+    }
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link');
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
+}
